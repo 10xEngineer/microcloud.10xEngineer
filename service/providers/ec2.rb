c906a04b522956d982c8ae57f92ae9245aa48f5c
Radim Marek <radim@laststation.net>
2012-06-05 22:48:07 +0200

    Basic EC2 start (without chef bootstrap)

--- service/providers/ec2.rb
+++ service/providers/ec2.rb
@@ -2,6 +2,24 @@ service_provider :ec2 do
   require 'fog'
 
   # TODO shared logic (fog connection), rails filter style
+
+  action :start do |request|
+    connection = Fog::Compute.new({
+      :provider => 'AWS',
+      :aws_secret_access_key => request["options"]["secret_access_key"],
+      :aws_access_key_id => request["options"]["access_key_id"],
+      :region => request["options"]["region"] || "us-east-1"
+    })
+
+    # TODO availability zone
+    server = connection.servers.create(:key_name => request["options"]["key"],
+                                          :image_id => request["options"]["ami"],
+                                          :flavor_id => request["options"]["type"])
+
+    # TODO use bootstrap instead (and run chef provisioning)
+
+    response :ok, :id => server.id
+  end
   
   action :stop do |request|
     raise "No server id provided." unless request["options"].include?("id")
