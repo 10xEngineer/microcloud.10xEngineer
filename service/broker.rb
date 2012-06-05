91fb55b8fd7e85d2cc85e4828741ef218eec4b1b
Radim Marek <radim@laststation.net>
2012-06-05 14:02:36 +0200

    Refactored message read part

--- service/broker.rb
+++ service/broker.rb
@@ -31,22 +31,27 @@ end
 # list of service sockets
 sockets = services.values.collect { |service| service[:socket] }
 
-# 0mq message protocol
-protocol = [:address, nil, :message]
+def read_message(socket)
+  zmq_format = [:address, nil, :message]
+
+  message = {}
+  position = 0
+  begin
+    socket.recv_string(raw_message = '')
+    message[zmq_format[position]] = raw_message unless zmq_format[position].nil?
+
+    position = position + 1
+  end while socket.more_parts?
+
+  message
+end
+
 
 loop do
   poller.poll(:blocking)
   poller.readables.each do |socket|
     if socket == frontend
-      request = {}
-      msg_pos = 0
-
-      begin 
-        socket.recv_string(raw_message = '')
-
-        request[protocol[msg_pos]] = raw_message unless protocol[msg_pos].nil?
-        msg_pos = msg_pos + 1
-      end while socket.more_parts?
+      request = read_message(socket)
 
       message = Yajl::Parser.parse(request[:message])
 
@@ -59,17 +64,8 @@ loop do
     elsif sockets.include?(socket)
       # response from registered service
       
-      # FIXME refactor message handling code
-      response = {}
-      msg_pos = 0
-
-      begin 
-        socket.recv_string(raw_message = '')
-
-        response[protocol[msg_pos]] = raw_message unless protocol[msg_pos].nil?
-        msg_pos = msg_pos + 1
-      end while socket.more_parts?
-
+      response = read_message(socket)
+      
       frontend.send_string response[:address], ZMQ::SNDMORE
       frontend.send_string '', ZMQ::SNDMORE
       frontend.send_string response[:message], 0
