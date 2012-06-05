d737b5d9509d1e88c7f50cd57f682c59ea05d8b3
Radim Marek <radim@laststation.net>
2012-06-05 22:33:37 +0200

    ec2 stop

--- service/providers/ec2.rb
+++ service/providers/ec2.rb
@@ -1,14 +1,32 @@
 service_provider :ec2 do
   require 'fog'
 
-  # TODO shared logic (fog connection)
+  # TODO shared logic (fog connection), rails filter style
+  
+  action :stop do |request|
+    raise "No server id provided." unless request["options"].include?("id")
 
-  action :status do |request|
     connection = Fog::Compute.new({
       :provider => 'AWS',
       :aws_secret_access_key => request["options"]["secret_access_key"],
       :aws_access_key_id => request["options"]["access_key_id"],
-      # TODO options / region
+      :region => request["options"]["region"] || "us-east-1"
+    })
+
+    server = connection.servers.get(request["options"]["id"])
+
+    server.destroy
+
+    response :ok
+  end
+
+  action :status do |request|
+    raise "No server id provided." unless request["options"].include?("id")
+
+    connection = Fog::Compute.new({
+      :provider => 'AWS',
+      :aws_secret_access_key => request["options"]["secret_access_key"],
+      :aws_access_key_id => request["options"]["access_key_id"],
       :region => request["options"]["region"] || "us-east-1"
     })
 
