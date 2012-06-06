bc2fed7e0cf3bacb456d21d9cfe8105f0b80b49a
Radim Marek <radim@laststation.net>
2012-06-06 09:08:34 +0200

    Exposed credentials by accident

--- service/test_command.rb
+++ service/test_command.rb
@@ -9,25 +9,25 @@ context = ZMQ::Context.new(1)
 socket = context.socket ZMQ::REQ
 
 # sample message
-#request = {
-#  :service => :dummy,
-#  :action => :ping,
-#  :options => {:say => "Hi!"}
-#}
-
 request = {
-  :service => :ec2,
-  :action => :stop,
-  :options => {
-    "secret_access_key" => "A5WK+zZbX/UMWTJ61XOVos/OFuYngEyaakQn8s7g",
-    "access_key_id" => "AKIAI3OXTTB6BTF4EPCQ",
-    "region" => "us-east-1",
-    #"ami" => "ami-3202f25b",
-    #"key" => "ec2-keypair",
-    #"type" => "t1.micro"
-    "id" => "i-0a11c573"
-  }
+  :service => :dummy,
+  :action => :ping,
+  :options => {:say => "Hi!"}
 }
+
+#request = {
+#  :service => :ec2,
+#  :action => :stop,
+#  :options => {
+#    #"secret_access_key" => "A5WK+zZbX/UMWTJ61XOVos/OFuYngEyaakQn8s7g",
+#    #"access_key_id" => "AKIAI3OXTTB6BTF4EPCQ",
+#    "region" => "us-east-1",
+#    #"ami" => "ami-3202f25b",
+#    #"key" => "ec2-keypair",
+#    #"type" => "t1.micro"
+#    #"id" => "i-0a11c573"
+#  }
+#}
 
 message = Yajl::Encoder.encode(request)
 
