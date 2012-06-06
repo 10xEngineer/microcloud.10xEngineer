1d1c82110ba499f013b4e7a84cdb3028d92f4561
Radim Marek <radim@laststation.net>
2012-06-06 10:35:58 +0200

    Upgraded outdated apt cookbook

--- a_vagrant_machine/cookbooks/apt/resources/preference.rb
+++ a_vagrant_machine/cookbooks/apt/resources/preference.rb
@@ -1,8 +1,8 @@
 #
 # Cookbook Name:: apt
-# Recipe:: cacher
+# Resource:: preference
 #
-# Copyright 2008-2009, Opscode, Inc.
+# Copyright 2010-2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,27 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-package "apt-cacher" do
-  action :install
+
+actions :add, :remove
+
+def initialize(*args)
+  super
+  @action = :add
 end
 
-service "apt-cacher" do
-  supports :restart => true, :status => false
-  action [ :enable, :start ]
-end
-
-cookbook_file "/etc/apt-cacher/apt-cacher.conf" do
-  source "apt-cacher.conf"
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :restart, resources(:service => "apt-cacher")
-end
-
-cookbook_file "/etc/default/apt-cacher" do
-  source "apt-cacher"
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :restart, resources(:service => "apt-cacher")
-end
+attribute :package_name, :kind_of => String, :name_attribute => true
+attribute :pin, :kind_of => String
+attribute :pin_priority, :kind_of => String
