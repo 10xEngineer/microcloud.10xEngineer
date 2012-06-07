041959bb078bc33685a7c004866745f80865aef0
Radim Marek <radim@laststation.net>
2012-06-07 10:56:38 +0200

    Spec of based container lifecycle commands

--- service/providers/lxc.rb
+++ service/providers/lxc.rb
@@ -1,2 +1,35 @@
 service_provider :lcx do
+
+  # container data
+  # 
+  # id (hash)
+  # state (stored outside)
+  # type
+  # server/pool reference
+  # descriptor (disk size, cgroups, firewall, etc). might come from course-lab-descriptor
+
+  action :prepare do
+    # prepare a single container
+    # arguments: id, type (what boostrap template to use), server/pool reference (for maintenance purposes), initial descriptor
+
+  end
+
+  action :allocate do
+    # allocate prepared container 
+    # arguments: id, course_template (how to finish the provisioning)
+  end
+
+  action :start do
+  end
+
+  action :stop do
+  end
+
+  action :status do
+    # return run-time information about the container 
+    # to-be used as part of API info command
+    #
+  end
+
+  # TODO whole migration/persistence commands will follow
 end
