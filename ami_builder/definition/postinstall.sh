6fbb877a463d1c54f7e52ca15c2cb2ae07b31406
Radim Marek <radim@laststation.net>
2012-06-10 15:51:03 +0200

    Fixed sudo and non interactive frontend for upgrade

--- ami_builder/definition/postinstall.sh
+++ ami_builder/definition/postinstall.sh
@@ -1,10 +1,12 @@
 #/bin/sh
 
 # basic dependencies
-apt-get -y update                                                               
-apt-get -y upgrade
-apt-get -y install ruby1.9.3 build-essential vim 
-apt-get clean
+sudo apt-get -y update                                                               
+# grub-pc non interactive options
+# http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+sudo apt-get -y install ruby1.9.3 build-essential vim 
+sudo apt-get clean
 
 # install chef
-/usr/bin/gem install chef --no-ri --no-rdoc 
+sudo /usr/bin/gem install chef --no-ri --no-rdoc 
