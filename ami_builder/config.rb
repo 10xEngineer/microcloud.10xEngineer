166509c14af7663d63b528433c1651f3056b48b1
Radim Marek <radim@laststation.net>
2012-06-10 19:46:32 +0200

    Image IDs of EBS back Ubuntu 12.04

--- ami_builder/config.rb
+++ ami_builder/config.rb
@@ -3,15 +3,15 @@ module TenxEngineer
   # list of based Ubuntu Images to use
   # http://cloud-images.ubuntu.com/releases/precise/release/
   #
-  # Disclaimer: use 64-bit instance-store AMIs only
+  # Disclaimer: use 64-bit EBS instances
   #
   SOURCE_AMI = {
-    "ap-northeast-1" => "ami-2cc7772d",
-    "ap-southeast-1" => "ami-a0ca8df2",
-    "eu-west-1" => "ami-1de8d369",
-    "sa-east-1" => "ami-96d8068b",
-    "us-east-1" => "ami-3c994355",
-    "us-west-1" => "ami-e7712aa2",
-    "us-west-2" => "ami-38800c08"
+    "ap-northeast-1" => "ami-60c77761",
+    "ap-southeast-1" => "ami-a4ca8df6",
+    "eu-west-1" => "ami-e1e8d395",
+    "sa-east-1" => "ami-8cd80691",
+    "us-east-1" => "ami-a29943cb",
+    "us-west-1" => "ami-87712ac2",
+    "us-west-2" => "ami-20800c10"
   }
 end
