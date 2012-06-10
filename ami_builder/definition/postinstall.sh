84e51a025223d823971ad9238e70db8f2e52a70f
Radim Marek <radim@laststation.net>
2012-06-10 15:34:28 +0200

    Basic Chef setup

--- ami_builder/definition/postinstall.sh
+++ ami_builder/definition/postinstall.sh
@@ -1,3 +1,10 @@
 #/bin/sh
 
-echo "TADAAA"
+# basic dependencies
+apt-get -y update                                                               
+apt-get -y upgrade
+apt-get -y install ruby1.9.3 build-essential vim 
+apt-get clean
+
+# install chef
+/usr/bin/gem install chef --no-ri --no-rdoc 
