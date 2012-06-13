d10e25f60ea5508e7b64ec7e8fb4a94f7ad9a417
Radim Marek <radim@laststation.net>
2012-06-13 13:01:55 +0200

    Using S3 for 10xeng_root distribution
    Need to come up with the release management for the hostnode-dist.tar.gz

--- service/providers/ec2.rb
+++ service/providers/ec2.rb
@@ -1,6 +1,12 @@
 require 'fog'
+require 'erb'
 
 class Ec2Service < Provider
+
+  BINARY_DIST = {
+    :bucket => 'ops-images',
+    :file => 'hostnode-dist.tar.gz'
+  }
 
   def start(request)
     connection = Fog::Compute.new({
@@ -10,16 +16,39 @@ class Ec2Service < Provider
       :region => request["options"]["region"] || "us-east-1"
     })
 
+    # TODO prepare temporary download URL (valid for 1 hour)
+    #      ? how to maintain multiple ec2 accounts and URL signature (most likely single URL?)
+    # TODO use current AWS account to sign it
+    # TODO and using ERB generate user_data
+   
+    # sign URL
+    s3 = Fog::Storage.new({
+      :provider => 'AWS',
+      :aws_secret_access_key => request["options"]["secret_access_key"],
+      :aws_access_key_id => request["options"]["access_key_id"],
+      :region => request["options"]["region"] || "us-east-1"
+    })
+
+    # TODO re-use signed URLs (general quite slow)
+    bucket = s3.directories.get(BINARY_DIST[:bucket])
+    target_file = bucket.files.get(BINARY_DIST[:file])
+
+    expiration = Time.now.utc + 60*15
+    download_url = target_file.url(expiration)
+
+    template = ERB.new(File.read(File.join(File.dirname(__FILE__), "../dist/10xeng-dist.sh.erb")))
+    user_data = template.result(binding)
+
     # TODO availability zone
     server = connection.servers.create(:key_name => request["options"]["key"],
-                                          :image_id => request["options"]["ami"],
-                                          :flavor_id => request["options"]["type"],
-                                          :user_data => File.read(File.join(File.dirname(__FILE__), "../dist/10xeng.sh")))
+                                       :image_id => request["options"]["ami"],
+                                       :flavor_id => request["options"]["type"],
+                                       :user_data => user_data )
 
     # TODO hostname is nil (might be good idea to create own hostname/DNS provisioning)
     response :ok, :id => server.id, :hostname => server.dns_name
   end
-  
+
   def stop(request)
     raise "No server id provided." unless request["options"].include?("id")
 
