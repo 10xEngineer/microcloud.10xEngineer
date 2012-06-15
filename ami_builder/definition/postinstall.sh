b5e4706517690b354d02e7dc9a0a40c3331a8b75
Radim Marek <radim@laststation.net>
2012-06-15 17:33:18 +0200

    Use Chef 10.12-rc1

--- ami_builder/definition/postinstall.sh
+++ ami_builder/definition/postinstall.sh
@@ -12,4 +12,4 @@ sudo apt-get -y install ruby1.9.3 build-essential vim
 sudo apt-get clean
 
 # install chef
-sudo /usr/bin/gem install chef --no-ri --no-rdoc 
+sudo /usr/bin/gem install chef --no-ri --no-rdoc --pre
