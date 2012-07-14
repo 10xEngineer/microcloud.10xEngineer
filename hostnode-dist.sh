94203923a63a52e3a38b1b2d61ba1f2b544149de
Radim Marek <radim@laststation.net>
2012-07-14 23:38:58 +0200

    Default Microcloud endpoint

--- hostnode-dist.sh
+++ hostnode-dist.sh
@@ -6,14 +6,16 @@ cd $DIR
 #TODO multi-region support 
 #     or switch to CF based distribution (check ec2 service)
 #TARGET=s3://ops-images/hostnode-dist.tar.gz
-TARGET=s3://tenx-labs-ops/hostnode-dist.tar.gz
+TARGET=s3://tenxlabs-ap-southeast-1/hostnode-dist.tar.gz
 DISTFILE=/tmp/hostnode-dist.tar.gz
 
 command -v s3cmd >/dev/null 2>&1 || { echo >&2 "s3tools are required to run hostnode distribution bundler. Come back when you have it installed."; exit 1; }
 
+# TODO hostnode.json to be configurable at hostnode distribution level
+#      or instance start ec2.rb user_data
 tar -cz -f $DISTFILE -X hostnode-dist.exclude ./
 
 s3cmd put $DISTFILE $TARGET
-#s3cmd setacl $TARGET --acl-public
+s3cmd setacl $TARGET --acl-public
 
 #rm $DISTFILE
