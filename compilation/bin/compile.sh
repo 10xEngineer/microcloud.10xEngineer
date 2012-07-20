750aaadddde6801644702a513dfca059ba194856
Radim Marek <radim@laststation.net>
2012-07-20 09:43:06 +0200

    Minor fixes

--- compilation/bin/compile.sh
+++ compilation/bin/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+ssh-add /home/tenx/.ssh/id_rsa
 bundle exec ruby compile.rb $@
--- compilation/compile.rb
+++ compilation/compile.rb
@@ -10,9 +10,8 @@ require 'definition/metadata'
 
 def prepare_repo(temp_dir, repo)
   repo = "ssh://#{repo}" unless repo.match /^ssh\:\/\//
-  puts "Cloning #{repo}"
 
-  git = Grit::Git.new(temp_dir)
+  git = Grit::Git.new(".")
 
   options = {
     :quiet => false,
@@ -26,39 +25,39 @@ def prepare_repo(temp_dir, repo)
   temp_dir
 end
 
-repo_dir = Dir.mktmpdir
+Dir.mktmpdir do |repo_dir|
+  # parse arguments
+  repo = ARGV.shift
+  lab_token = ARGV.shift
+  repo_rev = ARGV.shift
+  repo_ref = ARGV.shift
 
-# parse arguments
-repo = ARGV.shift
-lab_token = ARGV.shift
-repo_rev = ARGV.shift
-repo_ref = ARGV.shift
+  # get repository
+  prepare_repo(repo_dir, repo)
+  puts "Compile environment ready."
 
-# get repository
-prepare_repo(repo_dir, repo)
-puts "Compile environment ready."
+  # verify pre-requisuites
+  metadata_rb = File.join(repo_dir, "metadata.rb")
+  unless File.exists? metadata_rb
+    puts "Invalid lab definition: no metadata.rb"
 
-# verify pre-requisuites
-metadata_rb = File.join(repo_dir, 'metadata.rb')
-unless File.exists? metadata_rb
-  warn "Invalid lab definition: no metadata.rb"
+    # FIXME exit 1 aborts the push 
+    # FIXME need to find more elegant solution
+  else
+    # read metadata
+    m = Metadata.new(metadata_rb)
+    begin 
+      m.evaluate
+    rescue Exception => e
+      puts "Metadata evaluation failed: #{e.message}"
 
-  # FIXME exit 1 aborts the push 
-  # FIXME need to find more elegant solution
-else
-  # read metadata
-  m = Metadata.new(metadata_rb)
-  begin 
-    m.evaluate
-  rescue Exception => e
-    warn "Metadata evaluation failed: #{e.message}"
+      exit 99
+    end
 
-    exit 99
+    json_def = m.to_json
+    puts "Temporary definition output:"
+    puts json_def
   end
-
-  json_def = m.to_json
-  puts "Temporary definition output:"
-  puts json_def
 end
 
 # push to microcloud
@@ -69,6 +68,5 @@ end
 
 # FIXME ensure cleanup on failure too
 # remove fragments
-FileUtils.remove_entry_secure repo_dir
 puts "Compilation fragments removed."
 
