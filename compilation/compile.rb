b62cc1e9ce1314b568b3f29094b2f0e601339694
Radim Marek <radim@laststation.net>
2012-07-20 08:36:16 +0200

    Fixed repo format
    Already comes with full path

--- compilation/compile.rb
+++ compilation/compile.rb
@@ -8,12 +8,9 @@ require 'tmpdir'
 require 'fileutils'
 require 'definition/metadata'
 
-def prepare_repo(source)
-  # FIXME hardcoded for now
-  # FIXME hardcoded repo (should be resolved from name)
-  repo = "ssh://#{source}"
+def prepare_repo(temp_dir, repo)
+  puts "Cloning #{repo}"
 
-  temp_dir = Dir.mktmpdir
   git = Grit::Git.new(temp_dir)
 
   options = {
@@ -28,6 +25,8 @@ def prepare_repo(source)
   temp_dir
 end
 
+repo_dir = Dir.mktmpdir
+
 # parse arguments
 repo = ARGV.shift
 lab_token = ARGV.shift
@@ -35,7 +34,7 @@ repo_rev = ARGV.shift
 repo_ref = ARGV.shift
 
 # get repository
-repo_dir = prepare_repo(repo)
+prepare_repo(repo_dir, repo)
 puts "Compile environment ready."
 
 # verify pre-requisuites
@@ -43,22 +42,23 @@ metadata_rb = File.join(repo_dir, 'metadata.rb')
 unless File.exists? metadata_rb
   warn "Invalid lab definition: no metadata.rb"
 
-  exit 1
+  # FIXME exit 1 aborts the push 
+  # FIXME need to find more elegant solution
+else
+  # read metadata
+  m = Metadata.new(metadata_rb)
+  begin 
+    m.evaluate
+  rescue Exception => e
+    warn "Metadata evaluation failed: #{e.message}"
+
+    exit 99
+  end
+
+  json_def = m.to_json
+  puts "Temporary definition output:"
+  puts json_def
 end
-
-# read metadata
-m = Metadata.new(metadata_rb)
-begin 
-  m.evaluate
-rescue Exception => e
-  warn "Metadata evaluation failed: #{e.message}"
-
-  exit 99
-end
-
-json_def = m.to_json
-puts "Temporary definition output:"
-puts json_def
 
 # push to microcloud
 # FIXME how to lookup lab definition
@@ -66,7 +66,8 @@ puts json_def
 # => implicit authentication
 
 
+# FIXME ensure cleanup on failure too
 # remove fragments
-FileUtils.rm_rf repo_dir
+FileUtils.remove_entry_secure repo_dir
 puts "Compilation fragments removed."
 
