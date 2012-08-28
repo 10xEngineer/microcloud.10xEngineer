e7b96c64bae0fd5533f374c47d0a1a33d0752510
Radim Marek <radim@laststation.net>
2012-08-28 16:48:28 +0200

    Updated path for production deployment

--- lxc-boostrap/etc/10xlab/bootstrap.rb
+++ lxc-boostrap/etc/10xlab/bootstrap.rb
@@ -1,4 +1,4 @@
-cookbook_path "/tmp/var/10xlab/cookbooks"
-role_path "/tmp/var/10xlab/roles"
-data_bag_path "/tmp/var/10xlab/data_bags"
-file_cache_path "/tmp/var/10xlab"
+cookbook_path "/var/10xlab/cookbooks"
+role_path "/var/10xlab/roles"
+data_bag_path "/var/10xlab/data_bags"
+file_cache_path "/var/10xlab"
