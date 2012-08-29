d3c8d3edc16bfd821be714bab8903b2fe5ddb0ce
Radim Marek <radim@laststation.net>
2012-08-29 21:39:16 +0200

    LXC bootstrap service now uses local metadata to retrieve lab repository
    Private key is not longer necessary as the request to proxied via hostnode's endpoint at http://10.0.3.1/repository

--- lxc-boostrap/build.sh
+++ lxc-boostrap/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-fpm -s dir -t deb -n 10xlab-bootstrap -v 0.1 -a all --after-install local/postinst.sh --exclude local/* --exclude build.sh .
+fpm -s dir -t deb -n 10xlab-bootstrap -v 0.2 -a all --after-install local/postinst.sh --exclude local/* --exclude build.sh .
