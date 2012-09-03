ab6657ee9afc22c47bd0b07a750ef73f35b09b1d
Radim Marek <radim@laststation.net>
2012-09-03 15:56:07 +0200

    Node Serv package script

---
+++ node_serv/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+tmpdir=`mktemp -d /tmp/node_serv.XXXXXXXX` || exit 1
+echo "using $tmpdir"
+
+# prepare files
+mkdir -p ${tmpdir}/opt/10xlabs/node_serv
+cp *.coffee ${tmpdir}/opt/10xlabs/node_serv
+cp package.json ${tmpdir}/opt/10xlabs/node_serv
+
+fpm -s dir -t deb -d 'npm' -d 'nodejs' -n 10xlab-node_serv -v 0.1 -a all --after-install local/postinst.sh --exclude local/* --exclude build.sh -C $tmpdir .
+
+rm -Rf $tmpdir
