1d2a5f969f7f09a28cc0c3dd11edb9120afba40f
Radim Marek <radim@laststation.net>
2012-09-06 13:03:40 +0200

    Verbose 10xlabs-compile.sh

--- compilation/hooks/10xlabs-compile.sh
+++ compilation/hooks/10xlabs-compile.sh
@@ -6,6 +6,8 @@
 # * revision
 # * ref_name
 
+set -x -e
+
 . /home/git/.compile.conf
 
 ssh -A -o "StrictHostKeyChecking no" -i ~/.ssh/compile compile@$COMPILE_NODE "cd /home/compile/deploy/compilation && bin/compile.sh $@"
