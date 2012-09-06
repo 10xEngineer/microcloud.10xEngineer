5e915ea93d8b4512aa313f2977959d0219d9ea36
Radim Marek <radim@laststation.net>
2012-09-06 12:55:18 +0200

    Misc fixes

--- service/bin/service.sh
+++ service/bin/service.sh
@@ -2,6 +2,9 @@
 
 if [ -d "/home/microcloud" ]; then
 	export HOME=/home/microcloud
+	
+	# TODO refactor key distribution (comes from 10xeng-mc::default recipe)
+	# https://trello.com/card/re-factor-key-distribution/50067c2712a969ae032917f4/71
 	ssh-add /etc/10xlabs/mchammer
 fi
 
