3c1b0c8deb17494c229e6ff9dc047d1448581a8a
Radim Marek <radim@laststation.net>
2012-09-06 12:54:55 +0200

    Fixed whitespace

--- compilation/hooks/10xlabs-compile.sh
+++ compilation/hooks/10xlabs-compile.sh
@@ -6,7 +6,6 @@
 # * revision
 # * ref_name
 
-# FIXME hardcoded ip address of compilation node
 . /home/git/.compile.conf
 
-ssh -A -o "StrictHostKeyChecking no" -i ~/.ssh/compile compile@$COMPILE_NODE"cd /home/compile/deploy/compilation && bin/compile.sh $@"
+ssh -A -o "StrictHostKeyChecking no" -i ~/.ssh/compile compile@$COMPILE_NODE "cd /home/compile/deploy/compilation && bin/compile.sh $@"
