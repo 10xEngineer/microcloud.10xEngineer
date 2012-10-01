527162a98d782f0b10fa1f92ad02341defb2f412
Radim Marek <radim@laststation.net>
2012-10-01 10:16:41 +0200

    Basic ruby gems are now packaged as .deb packages
    Temporary provisioning instructions available at
    https://www.evernote.com/shard/s18/sh/ec770ffd-ec28-4e4c-8041-49af9ed244fb/71329bd3457c89ac6c978820d321951b

--- lxc-boostrap/build.sh
+++ lxc-boostrap/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x 
+
 bootstrap_gem_dir=../../10xlabs-bootstrap-handler
 
 if [ ! -d $bootstrap_gem_dir ]; then
@@ -17,4 +19,14 @@ cd $bootstrap_dir
 rm var/cache/10xlabs/*
 mv ${bootstrap_gem_dir}/10xlabs-bootstrap-handler-*.gem ./var/cache/10xlabs
 
-fpm -s dir -t deb -n 10xlab-bootstrap -v 0.4 -a all --after-install local/postinst.sh --exclude local/* --exclude build.sh .
+#
+# FIXME re-use pre-build deb packages
+# FIXME provide complete provisioning mechanism
+# https://www.evernote.com/shard/s18/sh/ec770ffd-ec28-4e4c-8041-49af9ed244fb/71329bd3457c89ac6c978820d321951b
+#
+#
+# TODO need on-demand provision as 'microcloud' gem tends to change quite a lot
+#
+cp local/debs/* ./var/cache/10xlabs
+
+fpm -s dir -t deb -n 10xlab-bootstrap -v 0.5 -a all --after-install local/postinst.sh --exclude "local/*" --exclude build.sh .
