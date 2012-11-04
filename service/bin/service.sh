3c3501f7613cef9d2da1782b24c2189b0df0ad3c
Radim Marek <radim@laststation.net>
2012-11-04 11:14:55 +0100

    Removed SSH agent in favour of default key ~/.ssh/id_rsa

--- service/bin/service.sh
+++ service/bin/service.sh
@@ -2,10 +2,6 @@
 
 if [ -d "/home/labsys" ]; then
 	export HOME=/home/labsys
-	
-	# TODO refactor key distribution (comes from 10xeng-mc::default recipe)
-	# https://trello.com/card/re-factor-key-distribution/50067c2712a969ae032917f4/71
-	ssh-add /etc/labs/mchammer
 fi
 
 exec bundle exec ruby service.rb $1
