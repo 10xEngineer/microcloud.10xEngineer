56cd4aa95a8b06520a5c4b98eedb534e5972ea97
Radim Marek <radim@laststation.net>
2012-11-04 10:46:07 +0100

    Updated deployment dir

--- service/bin/service.sh
+++ service/bin/service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ -d "/home/microcloud" ]; then
-	export HOME=/home/microcloud
+if [ -d "/home/labsys" ]; then
+	export HOME=/home/labsys
 	
 	# TODO refactor key distribution (comes from 10xeng-mc::default recipe)
 	# https://trello.com/card/re-factor-key-distribution/50067c2712a969ae032917f4/71
-	ssh-add /etc/10xlabs/mchammer
+	ssh-add /etc/labs/mchammer
 fi
 
 exec bundle exec ruby service.rb $1
