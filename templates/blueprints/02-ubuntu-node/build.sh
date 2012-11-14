25632e80939c592e92a08f2ea2ab152d443eabb1
Radim Marek <radim@laststation.net>
2012-11-14 22:01:22 +0000

    Node.js stack (w/mongo)

--- templates/blueprints/02-ubuntu-node/build.sh
+++ templates/blueprints/02-ubuntu-node/build.sh
@@ -4,13 +4,30 @@ set -x -e
 
 . shared/build-logic.sh
 
+TEMPLATE_NAME=nodejs-stack1
 BASE_IMAGE=01-ubuntu-precise
 TMPL_ROOT=`/bin/mktemp -d`
 ROOTFS=${TMPL_ROOT}/rootfs
+#ARCH=amd64
+ARCH=i386
 
 check_root
 mkdir -p rootfs
 
 copy_base_image $BASE_IMAGE $TMPL_ROOT
 
-chroot $ROOTFS apt-get install nodejs npm
+# node.js
+chroot $ROOTFS /usr/bin/apt-add-repository -y ppa:chris-lea/node.js
+chroot $ROOTFS apt-get -y update
+
+# mongodb
+chroot $ROOTFS apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+chroot $ROOTFS echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' >>$ROOTFS/etc/apt/sources.list
+
+chroot $ROOTFS apt-get -y update
+chroot $ROOTFS apt-get -y install nodejs npm 
+chroot $ROOTFS apt-get -y install mongodb-10gen || echo "mongodb-10gen configure failed as expected."
+
+create_archive $TMPL_ROOT $TEMPLATE_NAME $ARCH
+
+rm -Rf $TMPL_ROOT
