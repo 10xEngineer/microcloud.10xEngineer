a03dc1d410610d1add002808433155da60deca66
Radim Marek <radim@laststation.net>
2012-11-19 14:05:47 +0100

    Additional gems

--- templates/blueprints/03-ruby-base/build.sh
+++ templates/blueprints/03-ruby-base/build.sh
@@ -29,18 +29,29 @@ chroot $ROOTFS bash -c "cat /tmp/mysql.preseed | sudo debconf-set-selections"
 
 # dependencies
 chroot $ROOTFS apt-get -y update
-chroot $ROOTFS apt-get -y install zlib1g-dev libreadline-dev libxml2-dev libxslt1-dev libsqlite3-dev mysql-server libmysqlclient-dev libssl-dev imagemagick libmagickwand-dev
+chroot $ROOTFS apt-get -y install zlib1g-dev libreadline-dev libxml2-dev libxslt1-dev libsqlite3-dev mysql-server libmysqlclient-dev libssl-dev imagem
+agick libmagickwand-dev
 
 # ruby
 chroot $ROOTFS git clone git://github.com/sstephenson/rbenv.git /home/lab/.rbenv
-chroot $ROOTFS git clone git clone git://github.com/sstephenson/ruby-build.git /home/lab/.rbenv/plugins/ruby-build
-chroot $ROOTFS /bin/bash --userspec lab:lab -c "cd /home/lab ; rbenv install 1.9.3-p327"
+chroot $ROOTFS git clone git://github.com/sstephenson/ruby-build.git /home/lab/.rbenv/plugins/ruby-build
+chroot $ROOTFS /bin/bash -c "chown lab:lab -R /home/lab/.rbenv"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin rbenv install 1.9.3-p327"
+
+# ruby gems
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install rails"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install nokogiri"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install rmagick"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install rmagick"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install mysql2"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install sqlite3"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install pry"
+chroot --userspec lab:lab $ROOTFS /bin/bash -c "cd /home/lab ; HOME=/home/lab PATH=/home/lab/.rbenv/shims:/home/lab/.rbenv/bin:/bin:/usr/bin:/usr/sbin gem install sinatra"
 
 # start mysql by default
 chroot $ROOTFS -c bash "update-rc.d -f mysql-server remove"
 
-#rm $ROOTFS/usr/sbin/policy-rc.d
-
+rm $ROOTFS/usr/sbin/policy-rc.d
 # TODO profile.d
 
 # TODO gems
